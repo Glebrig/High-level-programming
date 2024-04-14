32d408f1f079930492d490565647596496f3edf4
Glebrig <89831824+Glebrig@users.noreply.github.com>
2024-04-14 15:42:27 +0300

    3 laba
    Удалил старые функции из 1 лабы

--- main.cpp
+++ main.cpp
@@ -15,63 +15,12 @@
 using namespace std;
 
 #pragma region функции-заглушки
-int f1() {
-    int theFirstNumber{};
-    int theSecondNumber{};
-    std::cout << "enter two numbers\n\n";
-    std::cin >> theFirstNumber >> theSecondNumber;
-    std::cout << "\nanswer: " << theFirstNumber + theSecondNumber << "\n\n";
-    return 1;
-}
-
-int f2() {
-    int theFirstNumber{};
-    int theSecondNumber{};
-    std::cout << "enter two numbers\n\n";
-    std::cin >> theFirstNumber >> theSecondNumber;
-    std::cout << "\nanswer: " << theFirstNumber - theSecondNumber << "\n\n";
-    return 2;
-}
-
-int f3() {
-    int theFirstNumber{};
-    int theSecondNumber{};
-    std::cout << "enter two numbers\n\n";
-    std::cin >> theFirstNumber >> theSecondNumber;
-    std::cout << "\nanswer: " << theFirstNumber * theSecondNumber << "\n\n";
-    return 3;
-}
-
-int f4() {
-    float theFirstNumber{};
-    float theSecondNumber{};
-    std::cout << "enter two numbers\n\n";
-    std::cin >> theFirstNumber >> theSecondNumber;
-    std::cout << "\nanswer: " << theFirstNumber / theSecondNumber << "\n\n";
-    return 4;
-}
-
-int f5(){
-    using namespace SGP;
-    Employee Gleb("Gleb", "Suhanov", 18, "sgp123", "123456789", "Pilot");
-    std::cout << Gleb;
-    std::cout << '\n';
-    return 5;
-}
-
-int f6(){
-    using namespace SGP;    
-    Airplane FirstPlane("12345", "MIG");
-    FirstPlane.printData();
-    std::cout << '\n';
-    return 6;
-}
 
 const int SIZE_ARRAY = 3;
 SGP::Employee arrayStaff[SIZE_ARRAY];
 SGP::Passenger arrayPassengers[SIZE_ARRAY];
 
-int f7(){
+int f1(){
     for(int i = 0; i<SIZE_ARRAY; i++){
        if(arrayStaff[i] != 0 )
        {
@@ -88,10 +37,10 @@ int f7(){
        }
     }
     std::cout <<'\n' << '\n';
-    return 7;
+    return 1;
 }
 
-int f8(){
+int f2(){
     int choice = 0;
     std::cout << "Who do you want to add?" << '\n' << "Employee (1)" << '\n' << "Passenger (2)" << '\n';
     std::cin >> choice;
@@ -122,10 +71,10 @@ int f8(){
         break;
     }
     std::cout <<'\n' << '\n';
-    return 8;
+    return 2;
 }
 
-int f9(){
+int f3(){
     int choice = 0;
     int index = -1;
     std::cout << "Who do you want to delete?" << '\n' << "Employee (1)" << '\n' << "Passenger (2)" << '\n';
@@ -148,10 +97,10 @@ int f9(){
         break;
     }
     std::cout <<'\n' << '\n';
-    return 9;
+    return 3;
 }
 
-int f10(){
+int f4(){
     int choice = 0;
     int direction = 0; 
     std::cout << "Who do you want to sort?" << '\n' << "Employee (1)" << '\n' << "Passenger (2)" << '\n';
@@ -253,19 +202,19 @@ int f10(){
 
     }
     std::cout <<'\n' << '\n';
-    return 10;
+    return 4;
 }
 
 
 #pragma endregion
 
-const int ITEMS_NUMBER = 10;
+const int ITEMS_NUMBER = 4;
 
 int main() {
   using namespace SGP;
     int a = 0;
-    CMenuItem items[ITEMS_NUMBER] {CMenuItem{"summation", f1}, CMenuItem{"subtraction", f2}, CMenuItem{"multiplication", f3}, CMenuItem{"division", f4}, CMenuItem{"employee", f5}, CMenuItem{"airplane", f6},
-    CMenuItem{"array of users", f7}, CMenuItem{"add user", f8}, CMenuItem{"delete user", f9}, CMenuItem{"sort users by age", f10}};
+    CMenuItem items[ITEMS_NUMBER] {CMenuItem{"array of users", f1}, CMenuItem{"add user", f2}, CMenuItem{"delete user", f3}, 
+    CMenuItem{"sort users by age", f4}};
     
     CMenu menu("My console menu", items, ITEMS_NUMBER);
     while (menu.runCommand()) {};
